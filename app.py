0c2b1990e8b0bc7597ed559b33c4389be1b11bfb
Muha2026 <muhapac12@gmail.com>
2026-01-14 02:04:22 -0800

    Update app.py

--- app.py
+++ app.py
@@ -44,12 +44,6 @@ if not licence_info or licence_info[0] != "Active":
         else:
             st.error("❌ Nom de boutique ou clé de licence invalide.")
     st.stop()# Arrête le code ici tant que ce n'est pas activé
-    if nom_boutique == "Kagenda Group" and licence_saisie == "PACY-102E-9198":
-    st.success("Activation réussie !")
-    st.session_state['active'] = True # ou la variable que vous utilisez
-    # Forcez l'enregistrement dans la base de données locale du serveur
-    c.execute("INSERT OR REPLACE INTO licence (nom, cle) VALUES (?, ?)", ("Kagenda Group", "PACY-102E-9198"))
-    conn.commit()
 
 # 2. Si activé, on affiche le nom de la boutique en haut
 nom_de_la_boutique = licence_info[1]
@@ -476,6 +470,7 @@ elif menu == "☎️ Aide & Support":
             st.success("Votre demande a été enregistrée. Pacy MHA vous contactera sous peu.")
 
    
+
 
 
 
